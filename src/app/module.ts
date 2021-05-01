394b8ab0b4d30c61bf04d3917ef57d7e135977f4
Mathias Remshardt <mathias.remshardt@logicline.de>
2021-05-01 12:52:44 +0200

    * removes not required routing

--- src/app/module.ts
+++ src/app/module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './component';
-import { AppRoutingModule } from './routing.module';
+import { UsingNgContentModule } from './ng-content/module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, UsingNgContentModule],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/ng-content/module.ts
+++ src/app/ng-content/module.ts
@@ -1,10 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { UsingNgContentComponent } from './components/component';
-import { UsingNgContentRoutingModule } from './routing.module';
 
 @NgModule({
   declarations: [UsingNgContentComponent],
-  imports: [CommonModule, UsingNgContentRoutingModule]
+  imports: [CommonModule],
+  exports: [UsingNgContentComponent]
 })
 export class UsingNgContentModule {}
