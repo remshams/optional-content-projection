eaed04fd633ac7eecad9b1374423eb4b81aaae15
Mathias Remshardt <mathias.remshardt@logicline.de>
2021-05-02 15:57:17 +0200

    * removes deprecated code in RenderSlotDirectiveComponent

--- src/app/ng-content/render-slot-directive/components/component.ts
+++ src/app/ng-content/render-slot-directive/components/component.ts
@@ -1,8 +1,4 @@
-import { ChangeDetectionStrategy, Component, ContentChildren, ElementRef, QueryList } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { SlotDirective } from '../../directives/directive';
-import { SlotName, SlotNames } from '../../directives/model';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-render-slot-directive',
@@ -10,25 +6,4 @@ import { SlotName, SlotNames } from '../../directives/model';
   styleUrls: ['./component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class RenderSlotDirectiveComponent {
-  @ContentChildren(SlotDirective, { read: ElementRef }) set slotDirectives(value: QueryList<ElementRef>) {
-    this.nativeSlots.next(Array.from(value));
-  }
-
-  private nativeSlots: BehaviorSubject<Array<ElementRef>>;
-  readonly slotNames$: Observable<SlotNames>;
-
-  constructor() {
-    this.nativeSlots = new BehaviorSubject<Array<ElementRef>>([]);
-
-    this.slotNames$ = this.setSlotsByName(this.nativeSlots.asObservable());
-  }
-
-  isSlotSet(slotName: SlotName): Observable<boolean> {
-    return this.slotNames$.pipe(map(slotNames => slotNames.includes(slotName)));
-  }
-
-  private setSlotsByName(slots$: Observable<Array<ElementRef>>): Observable<SlotNames> {
-    return slots$.pipe(map(slots => slots.map(slot => slot.nativeElement.getAttribute('slot'))));
-  }
-}
+export class RenderSlotDirectiveComponent {}
